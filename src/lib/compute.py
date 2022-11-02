1786c71ed87f33bf4cf7f03e392fdbd89bc50184
hverton1a <hverton1a@gmail.com>
2022-11-01 22:35:57 -0300

    Add support for multiplication

---
+++ src/lib/compute.py
@@ -0,0 +1,23 @@
+class Compute:
+    def __init__(self, operator, operands):
+        self.operator =  operator
+        self.operands = operands
+
+    def add(self):
+        pass
+
+    def subtract(self):
+        pass
+
+    def divide(self):
+        pass
+
+    def multiply(self):
+
+        sum = 1
+
+        for item in self.operands:
+
+            sum *= item
+
+        print(sum)
